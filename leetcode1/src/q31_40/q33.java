6e25a7b8b8dfd21282e88345f1e099999cace7ff
s061 <s061@172.26.152.46>
2015-01-28 14:50:51 +0800

    finish 33

--- leetcode1/src/q31_40/q33.java
+++ leetcode1/src/q31_40/q33.java
@@ -2,4 +2,13 @@ package q31_40;
 
 public class q33 {
 
+	public int search(int[] A, int target) {
+
+	      for (int i=0; i<A.length; i++)
+	      {
+	          if (A[i]==target) return i;
+	      }
+
+	      return -1;
+	    }
 }
