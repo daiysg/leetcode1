35093ea020bbe89bdba2178396846f4043be1e31
s061 <s061@172.26.152.46>
2015-01-28 16:08:39 +0800

    finish 35

--- leetcode1/src/q31_40/q35.java
+++ leetcode1/src/q31_40/q35.java
@@ -1,7 +1,16 @@
 package q31_40;
 
+//binary search
 public class q35 {
     public int searchInsert(int[] A, int target) {
-
+    	  int low = 0;
+    	  int high = A.length-1;
+          while(low<=high){
+              int mid = (low+high)/2;
+              if(A[mid] == target) return mid;
+              else if(A[mid] > target) high = mid-1;
+              else low = mid+1;
+          }
+          return low;
     }
 }
