d0291bcc03b58a349330949daaa40938c061c308
s061 <s061@172.26.152.46>
2015-01-29 17:55:40 +0800

    finish q38. simple quiz

--- leetcode1/src/q31_40/q38.java
+++ leetcode1/src/q31_40/q38.java
@@ -5,42 +5,46 @@ import java.util.Stack;
 
 public class q38 {
 
-public String countAndSay(int n) {
-        String result="1";
-        if (n==0) return "";
-        if (n==1) return result;
-        Stack<Integer> currentCount=new Stack<Integer>();
-    	Stack<Integer> newStack=new Stack<Integer>();
-        for (int i=0; i<n; i++)
-        {
-        	
-        	currentCount.push(1);
-        	newStack.push(result.charAt(0)-'0');
-        	for (int j=1; j<result.length(); j++)
-        	{
-        		if ((result.charAt(j)-'0')==newStack.peek())
-        		{
-        			Integer count=currentCount.pop()+1;
-        			currentCount.push(count);
-        		}
-        		
-        		else {
-        			currentCount.push(1);
-        			newStack.push(result.charAt(j)-'0');
-        		}
-        	}
-        	
-        	  StringBuilder sb=new StringBuilder();
-              while (!currentCount.empty())
-              {
-              	String current=currentCount.pop().toString()+newStack.pop().toString();
-              	sb.insert(0, current);
-              }
-              
-              result=sb.toString();
-        }
-        return result;        	
-    }
+	public static String countAndSay(int n) {
+		String result = "1";
+		if (n == 0)
+			return "";
+		if (n == 1)
+			return result;
+		Stack<Integer> currentCount = new Stack<Integer>();
+		Stack<Integer> newStack = new Stack<Integer>();
+		for (int i = 0; i < n-1; i++) {
 
+			currentCount.push(1);
+			newStack.push(result.charAt(0) - '0');
+			for (int j = 1; j < result.length(); j++) {
+				if ((result.charAt(j) - '0') == newStack.peek()) {
+					Integer count = currentCount.pop() + 1;
+					currentCount.push(count);
+				}
+
+				else {
+					currentCount.push(1);
+					newStack.push(result.charAt(j) - '0');
+				}
+			}
+
+			StringBuilder sb = new StringBuilder();
+			while (!currentCount.empty()) {
+				String current = currentCount.pop().toString()
+						+ newStack.pop().toString();
+				sb.insert(0, current);
+			}
+
+			result = sb.toString();
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(countAndSay(2));
+	}
 
 }
