39e5d292c2efa1e155d2be1e533a755694d6a7d3
s061 <s061@172.26.152.46>
2015-01-29 17:52:09 +0800

    start q38

---
+++ leetcode1/src/q31_40/q38.java
@@ -0,0 +1,46 @@
+package q31_40;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class q38 {
+
+public String countAndSay(int n) {
+        String result="1";
+        if (n==0) return "";
+        if (n==1) return result;
+        Stack<Integer> currentCount=new Stack<Integer>();
+    	Stack<Integer> newStack=new Stack<Integer>();
+        for (int i=0; i<n; i++)
+        {
+        	
+        	currentCount.push(1);
+        	newStack.push(result.charAt(0)-'0');
+        	for (int j=1; j<result.length(); j++)
+        	{
+        		if ((result.charAt(j)-'0')==newStack.peek())
+        		{
+        			Integer count=currentCount.pop()+1;
+        			currentCount.push(count);
+        		}
+        		
+        		else {
+        			currentCount.push(1);
+        			newStack.push(result.charAt(j)-'0');
+        		}
+        	}
+        	
+        	  StringBuilder sb=new StringBuilder();
+              while (!currentCount.empty())
+              {
+              	String current=currentCount.pop().toString()+newStack.pop().toString();
+              	sb.insert(0, current);
+              }
+              
+              result=sb.toString();
+        }
+        return result;        	
+    }
+
+
+}
