9a42ae81af0aaa0e1648a9c80f371828d8828277
daiysg <daiysg91@gmail.com>
2015-12-28 18:08:45 +0800

    finish q55

---
+++ src/main/java/q51_60/q55.java
@@ -0,0 +1,28 @@
+package q51_60;
+
+public class q55 {
+
+	
+public boolean canJump(int[] nums) {
+        
+		//int curCheck = 0;
+		int maxToGo = 0;	
+	
+		for (int i=0; i <nums.length; i++){		
+			if (maxToGo >= nums.length-1) return true;						
+			
+			if (maxToGo < i ) {
+				return false;
+			}
+			
+			 if (maxToGo < i+nums[i] ) {
+				 maxToGo = i+nums[i];
+			 }
+		}
+		
+		
+		if (maxToGo > nums.length-1) return true;	
+		else return false;
+		
+    } 
+}
