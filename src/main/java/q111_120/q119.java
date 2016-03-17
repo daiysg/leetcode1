e689bf5abc3283ad115358b8023ca2c39d2df8e5
daiysg <daiysg91@gmail.com>
2016-03-17 18:40:41 +0800

    finish q119

---
+++ src/main/java/q111_120/q119.java
@@ -0,0 +1,18 @@
+package q111_120;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class q119 {
+
+	 public List<Integer> getRow(int rowIndex) {
+	        List<Integer> res = new ArrayList<>();
+	        for(int i = 0; i <= rowIndex; i++) {
+	            res.add(1);
+	            for(int j = i-1; j > 0; j--) {
+	                res.set(j, res.get(j-1) + res.get(j));
+	            }
+	        }
+	        return res;
+	    }
+}
